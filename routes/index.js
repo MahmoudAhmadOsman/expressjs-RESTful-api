8993709b4db7ca1b8e2c87b630adcfa600efe603
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-05-10 22:59:42 -0500

    added author routes and crud functionalies

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Welcoome Express!" });
+  res.render("index", { title: "Welcoome to Alts Bookshop!" });
 });
 
 module.exports = router;
