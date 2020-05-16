e68df26e457b5e6f4143e7845218b0f1ec327aa4
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-05-16 00:23:14 -0500

    added registration and login routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Welcoome to Alts Bookshop!" });
+  res.render("index", { title: "Welcome to Alts Bookshop!" });
 });
 
 module.exports = router;
