a71a572578ef260647f81ae5af4161577f4d801a
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-05-17 21:52:55 -0500

    added mongodb cloud db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: "mongodb://localhost:27017/ejsapi",
+  database:
+    "mongodb+srv://atlasbookshop:atlasbookshop2020@cluster0-xtbzt.mongodb.net/test?retryWrites=true&w=majority",
   secret: "yoursecret",
 };
