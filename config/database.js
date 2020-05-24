76a438dd6018086d25c9468b26f86703ba0f3b38
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-05-23 23:06:24 -0500

    added login backend functionalities

--- config/database.js
+++ config/database.js
@@ -1,5 +1,11 @@
 module.exports = {
-  database: "mongodb://localhost:27017/ejsapi",
-
+  database:
+    "mongodb+srv://atlasbookshop:atlasbookshop2020@cluster0-xtbzt.mongodb.net/test?retryWrites=true&w=majority",
   secret: "yoursecret",
 };
+
+// module.exports = {
+//   database: "mongodb://localhost:27017/ejsapi",
+
+//   secret: "yoursecret",
+// };
