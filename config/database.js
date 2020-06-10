716abd7b098deccc9b0f0ea599981431bfd2d468
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-06-09 23:00:13 -0500

    fixed edit icon in show.ejs

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   database:
-    "mongodb+srv://atlasbookshop:atlasbookshop2020@cluster0-xtbzt.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://atlasbookshop:atlasbookshop2020@cluster0-xtbzt.mongodb.net/atlasbookshop?retryWrites=true&w=majority",
   secret: "yoursecret",
 };
