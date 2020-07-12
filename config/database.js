98ac4762379852bcc5202ca1611a7b18a78d23f8
About Me <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2020-07-12 16:24:16 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   database:
-    "mongodb+srv://atlasbookshop:atlasbookshop2020@cluster0-xtbzt.mongodb.net/atlasbookshop?retryWrites=true&w=majority",
+    "database_link_goes_here",
   secret: "yoursecret",
 };
